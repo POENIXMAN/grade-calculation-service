50df8f010d06bb1849a7a8a360a2b19661002bc5
POENIXMAN <userr100p@gmail.com>
2025-04-24 09:48:51 +0300

    added the class

--- src/main/java/ru/hpclab/hl/module1/service/SubjectCacheService.java
+++ src/main/java/ru/hpclab/hl/module1/service/SubjectCacheService.java
@@ -1,15 +1,13 @@
 package ru.hpclab.hl.module1.service;
 
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.hpclab.hl.module1.Configuration.SchoolJournalClientProperties;
 import ru.hpclab.hl.module1.DTO.SubjectDTO;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -29,13 +27,16 @@ public class SubjectCacheService {
     private Map<UUID, String> subjectCache = new HashMap<>();
     private long lastUpdateTime = 0;
 
+    private final ObservabilityService observabilityService;
+
     public SubjectCacheService(
             @Value("${subjectcache.printdelay}") int printDelay,
             RestTemplate restTemplate,
-            SchoolJournalClientProperties clientProperties) {
+            SchoolJournalClientProperties clientProperties, ObservabilityService observabilityService) {
         this.printDelay = printDelay;
         this.restTemplate = restTemplate;
         this.clientProperties = clientProperties;
+        this.observabilityService = observabilityService;
     }
 
     public Map<UUID, String> getSubjectCache() {
@@ -47,16 +48,21 @@ public class SubjectCacheService {
 
     public void refreshCache() {
         String subjectsUrl = clientProperties.getBaseUrl() + clientProperties.getEndpoints().getSubjects();
-        ResponseEntity<SubjectDTO[]> response = restTemplate.getForEntity(subjectsUrl, SubjectDTO[].class);
-        SubjectDTO[] allSubjects = response.getBody();
+        observabilityService.start("cache.subject.externalCall");
+        try {
+            ResponseEntity<SubjectDTO[]> response = restTemplate.getForEntity(subjectsUrl, SubjectDTO[].class);
+            SubjectDTO[] allSubjects = response.getBody();
 
-        if (allSubjects != null && allSubjects.length > 0) {
-            subjectCache = Arrays.stream(allSubjects)
-                    .collect(Collectors.toMap(
-                            SubjectDTO::getSubjectId,
-                            SubjectDTO::getClassName
-                    ));
-            lastUpdateTime = System.currentTimeMillis();
+            if (allSubjects != null && allSubjects.length > 0) {
+                subjectCache = Arrays.stream(allSubjects)
+                        .collect(Collectors.toMap(
+                                SubjectDTO::getSubjectId,
+                                SubjectDTO::getClassName
+                        ));
+                lastUpdateTime = System.currentTimeMillis();
+            }
+        } finally {
+            observabilityService.stop("cache.subject.externalCall");
         }
     }
 
